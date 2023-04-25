056cace4e57815d8908e56918305a6d3011521c1
nataliebrago <nata_9507@mail.ru>
2023-04-25 22:01:41 +0300

    homework1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     /*My
-      homework1
+      homework2
      */
-    public static void main(String[] args) {
-        //this comment includes my name and surname
-        System.out.println("Natalie" + " " + "Brago" + ".Welcome " +
-                "to my house.");
+    public static void main(String[] args)
+    {
+        System.out.print("Hello!");
+        System.out.println();
+
+        //Output my name and surname
+        System.out.println("My name is" + " Natalie");
+        System.out.println("and my surname" + " Brago");
+        System.out.println("I am " + (23 + 4) + " years old");
+        System.out.println("My hobby is singing");
+        System.out.println("Welcome " + "to my github.");
+
+        // This is an example comment for Git
+        System.out.println();
+        System.out.print("Please, enter your name & surname: ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input text " +   scanner.nextLine());
+
+
+        String name = "123";
+        int age = 34;
+        double weight = 88.1;
+        char symbol = 'A';
     }
 }
