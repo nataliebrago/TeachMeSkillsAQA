390ac2d7c7b4600a3e8c4f266371e70fc4b61312
nataliebrago <nata_9507@mail.ru>
2023-04-25 21:18:15 +0300

    homework1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
+    //Домашнее задание 1
     public static void main(String[] args) {
         //Выводим значение Hello world! в консоль
         System.out.printf("Hello world!");
