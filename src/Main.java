38fcd1f6e042060df988b296e00a0b6bfcc1879f
nataliebrago <nata_9507@mail.ru>
2023-04-30 17:59:34 +0300

    homework1

--- src/Main.java
+++ src/Main.java
@@ -17,15 +17,30 @@ public class Main {
         System.out.println("Welcome " + "to my github.");
 
         // This is an example comment for Git
-        System.out.println();
+        /*  System.out.println();
         System.out.print("Please, enter your name & surname: ");
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Input text " +   scanner.nextLine());
+        System.out.print("Input text " +   scanner.nextLine());*/
 
 
         String name = "123";
         int age = 34;
         double weight = 88.1;
         char symbol = 'A';
+
+
+        System.out.println("    |    |");
+        System.out.println("____|____|____");
+        System.out.println("    |    |");
+        System.out.println("____|____|____");
+        System.out.println("    |    |");
+        System.out.println("    |    |");
+
+        System.out.println("     *");
+        System.out.println("    ***");
+        System.out.println("   *****");
+        System.out.println("  *******");
+        System.out.println(" *********");
+
     }
 }
